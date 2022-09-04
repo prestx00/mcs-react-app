56e88f1573f46e7370ea0446bde192931cdcd03c
prestx00 <90870438+prestx00@users.noreply.github.com>
2022-09-04 13:42:03 +0300

    add loader & fix list

--- app3/src/Shop.js
+++ app3/src/Shop.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import Item from "./Item";
 export default function Shop() {
   const [list, setList] = useState();
+  const [loader, setLoader] = useState(false);
 
   useEffect(() => {
     (async () => {
+      setLoader(true)
       try {
         const response = await fetch("https://covid-shop-mcs.herokuapp.com");
         const data = await response.json();
@@ -12,13 +14,18 @@ export default function Shop() {
         setList(data);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoader(false)
       }
     })();
   }, []);
 
+  if(loader){
+    return <p>Загрузка...</p>
+  }
   return (
     <div className="shop">
-      {list && list.map((item) => <Item info={item} />)}
+      {!!list && list.map((item) => <Item info={item} />)}
     </div>
   );
 }
